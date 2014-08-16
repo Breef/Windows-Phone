56064ca380113e46f5147a66a49822f36fa986d6
cpiggott <cpiggott@ksu.edu>
2014-08-16 12:34:27 -0500

    Commit for the start of day two

--- Breef/Breef/ViewModels/MainViewModel.cs
+++ Breef/Breef/ViewModels/MainViewModel.cs
@@ -12,13 +12,13 @@ namespace Breef.ViewModels
     {
         public MainViewModel()
         {
-            this.Items = new ObservableCollection<Snippet>();
+            this.Stories = new ObservableCollection<Story>();
         }
 
         /// <summary>
         /// A collection for ItemViewModel objects.
         /// </summary>
-        public ObservableCollection<Snippet> Items { get; private set; }
+        public ObservableCollection<Story> Stories { get; private set; }
 
         private string _sampleProperty = "Sample Runtime Property Value";
         /// <summary>
@@ -63,97 +63,18 @@ namespace Breef.ViewModels
         /// </summary>
         public async void LoadData()
         {
-            var query = ParseObject.GetQuery("Stories").WhereLessThanOrEqualTo("createdAt", DateTime.Now.ToUniversalTime()).OrderBy("createdAt").OrderByDescending("createdAt");
-            query = query.Limit(30);
+            var query = ParseObject.GetQuery("Stories").WhereLessThanOrEqualTo("updatedAt", DateTime.Now.ToUniversalTime()).OrderBy("updatedAt").OrderByDescending("updatedAt");
+            query = query.Limit(10);
             IEnumerable<ParseObject> results = await query.FindAsync();
 
             int i = 0;
-            List<Snippet> stories = new List<Snippet>();
+            List<Story> storiesInput = new List<Story>();
 
             foreach (ParseObject story in results)
             {
 
-                //All try catches for relevant story information Uncheck Region to view
-                #region
-                string title, info, category, linkToImage;
-                int storyType;
-                DateTime createdOn, endDate, updatedAt;
-
-                try
-                {
-                    title = story.Get<string>("Title");
-                }
-                catch
-                {
-                    title = "";
-                }
-
-                try
-                {
-                    info = story.Get<string>("info");
-                }
-                catch
-                {
-                    info = "";
-                }
-
-                try
-                {
-                    category = story.Get<string>("category");
-                }
-                catch
-                {
-                    category = "";
-                }
-
-                try
-                {
-                    linkToImage = story.Get<string>("linkToImage");
-                }
-                catch
-                {
-                    linkToImage = "";
-                }
-
-                try
-                {
-                    storyType = story.Get<int>("storyType");
-                }
-                catch
-                {
-                    storyType = 0;
-                }
-
-                try
-                {
-                    createdOn = story.Get<DateTime>("createdOn");
-                }
-                catch
-                {
-                    createdOn = DateTime.Now;
-                }
-
-                try
-                {
-                    updatedAt = story.Get<DateTime>("updatedAt");
-                }
-                catch
-                {
-                    updatedAt = DateTime.Now;
-                }
-
-                try
-                {
-                    endDate = story.Get<DateTime>("endDate");
-                }
-                catch
-                {
-                    endDate = DateTime.Now;
-                }
-
-                #endregion
-
-                this.Items.Add(new Snippet() { SnippetID = i.ToString(), Title = title, Info = info, Category = category, ImageLink = linkToImage, StoryType = storyType, CreatedOn = createdOn, UpdatedAt = updatedAt, EndDate = endDate });
+                storiesInput.Add
+                //storiesInput.Add(new Snippet() { SnippetID = i.ToString(), Title = title, Info = info, Category = category, ImageLink = linkToImage, StoryType = storyType, CreatedOn = createdOn, UpdatedAt = updatedAt, EndDate = endDate });
                 i++;
 
             }
